658e1084e3f9fa43b8280313d696377b376ce856
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-03-07 18:35:23 -0800

    handling form submission

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
+const form = document.querySelector('form');
 
 const editPlayer1Btn = document.getElementById('edit-player-1-btn');
 const editPlayer2Btn = document.getElementById('edit-player-2-btn');
@@ -11,3 +12,5 @@ editPlayer2Btn.addEventListener('click', openPlayerConfig);
 
 cancelConfigBtn.addEventListener('click', closePlayerConfig );
 backdrop.addEventListener('click', closePlayerConfig);
+
+form.addEventListener(' submit', savePlayerConfig)
--- scripts/config.js
+++ scripts/config.js
@@ -8,3 +8,11 @@ function closePlayerConfig(){
   playerConfigOverlay.style.display = 'none';
   backdrop.style.display = 'none';
 }
+
+function savePlayerConfig(event){
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername').trim();
+  
+  
+}
