b783dfe6229d11b00b09303122b51b6778d539b5
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-03-07 22:03:11 -0800

    validating user input

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
 const form = document.querySelector('form');
+const errorsOutput = document.getElementById('config-errors');
 
 const editPlayer1Btn = document.getElementById('edit-player-1-btn');
 const editPlayer2Btn = document.getElementById('edit-player-2-btn');
 const cancelConfigBtn = document.getElementById('cancel-config-btn');
-
 
 editPlayer1Btn.addEventListener('click', openPlayerConfig);
 editPlayer2Btn.addEventListener('click', openPlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -7,12 +7,19 @@ function openPlayerConfig(){
 function closePlayerConfig(){
   playerConfigOverlay.style.display = 'none';
   backdrop.style.display = 'none';
+  form.firstElementChild.classList.remove('error')
+  errorsOutput.textContent = '';
 }
 
 function savePlayerConfig(event){
   event.preventDefault();
   const formData = new FormData(event.target);
   const enteredPlayername = formData.get('playername').trim();
-  
-  
+
+  if(!enteredPlayername ){
+    event.target.firstElementChild.classList.add('error')
+    errorsOutput.textContent = 'Please enter a valid name';
+    return;
+  }
 }
+
