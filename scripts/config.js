4de09b08fae49e5cd8a229c28806a63c89b8e671
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-03-07 22:41:48 -0800

    storing and managing submitted data

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
-
-function openPlayerConfig(){
+function openPlayerConfig(event){
+  editedPlayer = +event.target.dataset.playerid;
   playerConfigOverlay.style.display = 'block';
   backdrop.style.display = 'block';
 }
@@ -9,6 +9,7 @@ function closePlayerConfig(){
   backdrop.style.display = 'none';
   form.firstElementChild.classList.remove('error')
   errorsOutput.textContent = '';
+  form.firstElementChild.lastElementChild.value = '';
 }
 
 function savePlayerConfig(event){
@@ -21,5 +22,12 @@ function savePlayerConfig(event){
     errorsOutput.textContent = 'Please enter a valid name';
     return;
   }
+  
+  const updatedPlayerData = document.getElementById('player-'+ editedPlayer + '-data');
+  updatedPlayerData.children[1].textContent = enteredPlayername;
+  
+  players[editedPlayer -1].name = enteredPlayername;
+
+  closePlayerConfig();
 }
 
